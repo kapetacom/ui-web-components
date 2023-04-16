c2e5a7a028a0925b3afb0c5d85ebb27ea963154c
Henrik Hofmeister <henrik@newdawn.dk>
2023-04-16 12:35:41 +0200

    Fix logo

---
+++ stories/special.stories.tsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import { Logo } from '../src';
+
+export default {
+    title: 'Special',
+};
+
+export const LogoView = () => {
+    return (
+        <>
+            <div style={{ backgroundColor: '#333', width: 300, height: 30 }}>
+                <Logo light={true} />
+            </div>
+            <div style={{ backgroundColor: '#CCC', width: 300, height: 30 }}>
+                <Logo />
+            </div>
+            <div style={{ backgroundColor: '#333', width: 300, height: 30 }}>
+                <Logo light={true} width={150} height={30} />
+            </div>
+            <div style={{ backgroundColor: '#CCC', width: 300, height: 30 }}>
+                <Logo width={150} height={30} />
+            </div>
+        </>
+    );
+};
