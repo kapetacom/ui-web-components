0671c2358416ee39fa8e0dfcf945e44f15e38026
andertun <harald.andertun@gmail.com>
2023-06-29 21:35:27 +0200

    feat: Add Autocomplete component

---
+++ src/form/inputs/Autocomplete.tsx
@@ -0,0 +1,32 @@
+import { default as MuiAutocomplete, AutocompleteProps as MuiAutocompleteProps } from '@mui/material/Autocomplete';
+import { default as MuiTextField, TextFieldProps as MuiTextFieldProps } from '@mui/material/TextField';
+
+import React from 'react';
+
+export type AutocompleteProps<Option> = Omit<MuiAutocompleteProps<Option, boolean, boolean, boolean>, 'renderInput'> &
+    Pick<MuiTextFieldProps, 'name' | 'label' | 'placeholder'> & {
+        hidePopupIndicator?: boolean;
+    };
+
+export default function Autocomplete<Option>(props: AutocompleteProps<Option>) {
+    const { name, label, placeholder, hidePopupIndicator, autoFocus, ...autocompleteProps } = props;
+    return (
+        <MuiAutocomplete
+            {...autocompleteProps}
+            renderInput={(renderInputParams) => (
+                <MuiTextField
+                    {...renderInputParams}
+                    name={name}
+                    label={label}
+                    placeholder={placeholder}
+                    autoFocus={autoFocus}
+                    variant="standard"
+                    InputProps={{
+                        ...renderInputParams.InputProps,
+                        ...(hidePopupIndicator ? { endAdornment: null } : {}),
+                    }}
+                />
+            )}
+        />
+    );
+}
