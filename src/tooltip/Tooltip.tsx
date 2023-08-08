a132b3ff1c02bcbf7ef7b2239617ae0c2f8ce1ce
andertun <harald.andertun@gmail.com>
2023-08-08 13:06:18 +0200

    feat: Add a Tooltip component with custom styling

---
+++ src/tooltip/Tooltip.tsx
@@ -0,0 +1,22 @@
+import { Tooltip as MuiTooltip, TooltipProps as MuiTooltipProps, styled, tooltipClasses } from '@mui/material';
+import React from 'react';
+
+type TooltipProps = MuiTooltipProps & {
+    maxWidth?: number;
+};
+
+export const Tooltip = styled(({ className, ...props }: TooltipProps) => (
+    <MuiTooltip {...props} classes={{ popper: className }} />
+))(({ theme, maxWidth }) => ({
+    [`& .${tooltipClasses.arrow}::before`]: {
+        color: '#ffffff',
+    },
+    [`& .${tooltipClasses.tooltip}`]: {
+        backgroundColor: '#ffffff',
+        color: 'rgba(0, 0, 0, 0.87)',
+        maxWidth: maxWidth ?? 220,
+        fontSize: theme.typography.pxToRem(12),
+        boxShadow: '0px 0px 26px 0px rgba(5, 9, 13, 0.16)',
+        padding: theme.spacing(1, 2),
+    },
+}));
