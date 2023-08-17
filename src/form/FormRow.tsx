97955d99ea90ae11c0f97fee76cd37f275b5c8fd
Jacob Wejendorp <jacob@wejendorp.dk>
2023-08-17 16:10:51 +0200

    feat: allow container level validators in forms [KAP-712]

--- src/form/FormRow.tsx
+++ src/form/FormRow.tsx
@@ -73,7 +73,8 @@ export const FormRow = (props: FormRowProps) => {
     const [touchedState, setTouchedState] = useState(false);
     const [errorMessage, setErrorMessage] = useState('');
 
-    const validators = useMemo(() => normaliseValidators(props.validation), [props.validation]);
+    const validators = useMemo(() => normaliseValidators(props.validation).concat(context.validators), [props.validation, context.validators]);
+
 
     const touched = useMemo(() => {
         if (touchedState) {
