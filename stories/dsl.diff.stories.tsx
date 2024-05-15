af7ea3a9d58d05bd450fc3979a693301109dec97
andertun <harald.andertun@gmail.com>
2024-05-15 09:15:27 +0200

    test: Make entities used in storybook bigger

--- stories/dsl.diff.stories.tsx
+++ stories/dsl.diff.stories.tsx
@@ -1,29 +1,99 @@
 import React from 'react';
 
 import { DSLDiffEditor } from '../src/dsl/DiffEditor';
-import { DSLDataType, DSLEntity, DSLEntityType } from '@kapeta/kaplang-core';
+import { DSLDataType, DSLEntity, DSLEntityType, DSLEnum } from '@kapeta/kaplang-core';
 import _ from 'lodash';
 
 export default {
     title: 'DSL Diffing',
 };
 
-const entA: DSLDataType[] = [
+const entitiesA: DSLEntity[] = [
+    {
+        type: DSLEntityType.ENUM,
+        name: 'PostStatus',
+        description: 'Enum values for a posts status',
+        values: ['PUBLISHED', 'DRAFT', 'UNKNOWN'],
+    } satisfies DSLEnum,
     {
         type: DSLEntityType.DATATYPE,
-        name: 'Derp',
+        name: 'Post',
+        description: 'A post object',
         properties: [
             {
-                name: 'fisk',
+                name: 'id',
+                type: 'string',
+            },
+            {
+                name: 'tags',
+                type: { name: 'string', list: true, generics: [] },
+            },
+            {
+                name: 'metadata',
+                optional: true,
+                type: { name: 'Map', generics: ['string', 'string'] },
+            },
+            {
+                name: 'text',
+                type: 'string',
+            },
+        ],
+    } satisfies DSLDataType,
+    {
+        type: DSLEntityType.DATATYPE,
+        name: 'User',
+        description: 'A user object',
+        properties: [
+            {
+                name: 'id',
+                type: 'string',
+            },
+            {
+                name: 'name',
+                type: 'string',
+            },
+            {
+                name: 'email',
+                type: 'string',
+            },
+        ],
+    },
+    {
+        type: DSLEntityType.DATATYPE,
+        name: 'Comment',
+        description: 'A comment object',
+        properties: [
+            {
+                name: 'id',
+                type: 'string',
+            },
+            {
+                name: 'postId',
+                type: 'string',
+            },
+            {
+                name: 'text',
                 type: 'string',
             },
         ],
     },
 ] as const;
 
-const entB = _.cloneDeep(entA);
-entB[0].properties[0].type = 'number';
+const entitiesB = _.cloneDeep(entitiesA);
+
+// Change things in entitiesB
+const firstEntityB = entitiesB[0] as DSLEnum;
+firstEntityB.values[2] = 'DELETED';
+
+const secondEntityB = entitiesB[1] as DSLDataType;
+if (secondEntityB.properties) {
+    secondEntityB.properties[0].name = 'postId';
+}
+secondEntityB.properties?.push({
+    name: 'newProperty',
+    type: 'string',
+});
 
 export const demo = () => {
-    return <DSLDiffEditor entitiesA={entA} entitiesB={entB} readOnly />;
+    return <DSLDiffEditor entitiesA={entitiesA} entitiesB={entitiesB} readOnly />;
 };
