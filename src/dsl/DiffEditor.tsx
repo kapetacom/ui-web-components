20edbdd93754ae814f3784bd19af9e0ad18b4f5a
andertun <harald.andertun@gmail.com>
2024-05-15 09:14:24 +0200

    fix: Move options to constant and tweak a few things

--- src/dsl/DiffEditor.tsx
+++ src/dsl/DiffEditor.tsx
@@ -3,12 +3,13 @@
  * SPDX-License-Identifier: MIT
  */
 
-import React from 'react';
+import React, { useMemo } from 'react';
 
 import { DiffEditor } from '@monaco-editor/react';
 import { DSLEntity } from '@kapeta/kaplang-core';
 import { DSL_LANGUAGE_ID } from './types';
 import { DSLWriter } from '..';
+import { editor } from 'monaco-editor';
 
 export interface DiffEditorProps {
     entitiesA?: DSLEntity[];
@@ -20,16 +21,26 @@ export const DSLDiffEditor = (props: DiffEditorProps) => {
     const original = DSLWriter.write(props.entitiesA || []);
     const modified = DSLWriter.write(props.entitiesB || []);
 
+    const options = useMemo(() => {
+        return {
+            readOnly: !!props.readOnly,
+            lineNumbersMinChars: 3,
+            folding: true,
+            roundedSelection: false,
+            automaticLayout: true,
+            lineDecorationsWidth: 5,
+            wordWrap: 'off',
+            wordWrapColumn: 120,
+            scrollBeyondLastLine: false,
+            hideCursorInOverviewRuler: true,
+            overviewRulerLanes: 3,
+            overviewRulerBorder: true,
+        } satisfies editor.IStandaloneEditorConstructionOptions;
+    }, [props.readOnly]);
+
     return (
         <div className={'dsl-editor'}>
-            <DiffEditor
-                language={DSL_LANGUAGE_ID}
-                original={original}
-                modified={modified}
-                options={{
-                    readOnly: !!props.readOnly,
-                }}
-            />
+            <DiffEditor language={DSL_LANGUAGE_ID} original={original} modified={modified} options={options} />
         </div>
     );
 };
