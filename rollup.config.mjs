127a6fe815526bd2b3b3f23c857ad7376a18bb35
Jacob Wejendorp <jacob@wejendorp.dk>
2025-02-20 11:53:04 +0100

    perf: bundle w/ preserveModules + pure to enable treeshaking (#240)

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -6,9 +6,24 @@ import css from 'rollup-plugin-styler';
 import { dts } from 'rollup-plugin-dts';
 import sizes from 'rollup-plugin-sizes';
 import { visualizer } from 'rollup-plugin-visualizer';
-import bundleSize from 'rollup-plugin-bundle-size';
 import json from '@rollup/plugin-json';
 import packageJson from './package.json' with { type: 'json' };
+
+const plugins = (/** @type {string} */ dir) => [
+  external(),
+  json(),
+  resolve(),
+  commonjs(),
+  typescript({
+      tsconfig: './tsconfig.build.json',
+      outDir: dir,
+      declarationDir: dir,
+  }),
+  css(),
+  visualizer(),
+  sizes(),
+];
+
 
 export default [
     {
@@ -16,38 +31,37 @@ export default [
         external: ['monaco-editor'],
         output: [
             {
-                file: 'dist/cjs/index.js',
+                dir: 'dist/cjs',
+                preserveModules: true,
+                preserveModulesRoot: './src',
                 format: 'cjs',
                 name: packageJson.name,
                 generatedCode: 'es2015',
                 compact: false,
                 sourcemap: false,
             },
-            {
-                file: 'dist/esm/index.js',
-                format: 'esm',
-                name: packageJson.name,
-                generatedCode: 'es2015',
-                compact: false,
-                sourcemap: false,
-            },
         ],
-        plugins: [
-            external(),
-            json(),
-            resolve(),
-            commonjs(),
-            typescript({
-                tsconfig: './tsconfig.build.json',
-            }),
-            css(),
-            visualizer(),
-            bundleSize(),
-            sizes(),
-        ],
+        plugins: plugins('dist/cjs')
     },
     {
-        input: 'dist/cjs/index.d.ts',
+      input: 'src/index.ts',
+      external: ['monaco-editor'],
+      output: [
+          {
+              dir: 'dist/esm',
+              preserveModules: true,
+              preserveModulesRoot: './src',
+              format: 'esm',
+              name: packageJson.name,
+              generatedCode: 'es2015',
+              compact: false,
+              sourcemap: false,
+          },
+      ],
+      plugins: plugins('dist/esm'),
+  },
+    {
+        input: 'dist/esm/index.d.ts',
         output: [{ file: 'dist/index.d.ts', format: 'esm' }],
         external: [/\.(css|less)$/],
         plugins: [dts()],
